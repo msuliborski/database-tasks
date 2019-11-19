026d5ae7f0d3308256712cd6b43372657b345441
Michał Suliborski <msuliborski@gmail.com>
2019-11-19 08:54:24 +0100

    wdaw

--- database-programming/task-lab-04-pl-sql-introduction.sql
+++ database-programming/task-lab-04-pl-sql-introduction.sql
@@ -4,14 +4,14 @@ SET SERVEROUTPUT ON;
 
 -- 3. Create an anonymous block that prints (displays) "Hi, it's me" (you can print strings, DATEs and NUMBERs, for  others: TO_CHAR (.....)). Use the DBMS_OUTPUT package.
 BEGIN
-  dbms_output.PUT_LINE('Hi, it''s me ');
+    dbms_output.PUT_LINE('Hi, it''s me ');
 END;
 
 -- 4. Create an anonymous block that includes: declaration of a numeric variable, assigning a value and printing the value (VARIABLE = ......)
 DECLARE
-  v_vaw NUMBER(10) := 15;
+    v_vaw NUMBER(10) := 15;
 BEGIN
-  dbms_output.PUT_LINE(v_vaw);
+    dbms_output.PUT_LINE(v_vaw);
 END;
 
 -- 5. Create an anonymous block with 2 variables that prints (displays) a greeting, today’s date and tomorrow’s date. Sample output is as follows:
@@ -19,26 +19,26 @@ END;
     -- Today is: 24-OCT-2019
     -- Tomorrow is 25-OCT-2019
 DECLARE
-  today DATE := SYSDATE;
-  tomorrow DATE := SYSDATE + 1;
+    today DATE := SYSDATE;
+    tomorrow DATE := SYSDATE + 1;
 BEGIN
-  dbms_output.put_line('Hello, Agnieszka');
-  dbms_output.put_line('Today is: ' || today);
-  dbms_output.put_line('Tomorrow is: ' || tomorrow);
+    dbms_output.put_line('Hello, Agnieszka');
+    dbms_output.put_line('Today is: ' || today);
+    dbms_output.put_line('Tomorrow is: ' || tomorrow);
 END; 
 -- 6. Change the line that prints “Hello …” to print “Hello” and the first name of the employee, which identifier is provided by the user. Include his/her salary in the output.
 DECLARE
-  today DATE := SYSDATE;
-  tomorrow DATE := SYSDATE + 1;
-  emp_name VARCHAR2(50);
-  emp_sal NUMBER;
+    today DATE := SYSDATE;
+    tomorrow DATE := SYSDATE + 1;
+    emp_name VARCHAR2(50);
+    emp_sal NUMBER;
 BEGIN
-  select first_name, salary into emp_name, emp_sal from hr.employees
-  where  employee_id = 101; -- $emp_id instead of 101
-  dbms_output.put_line('Hello, ' || emp_name);
-  dbms_output.put_line('Today is: ' || today);
-  dbms_output.put_line('Tomorrow is: ' || tomorrow);
-  dbms_output.put_line('Your salary is: ' || emp_sal);
+    select first_name, salary into emp_name, emp_sal from hr.employees
+    where  employee_id = 101; -- $emp_id instead of 101
+    dbms_output.put_line('Hello, ' || emp_name);
+    dbms_output.put_line('Today is: ' || today);
+    dbms_output.put_line('Tomorrow is: ' || tomorrow);
+    dbms_output.put_line('Your salary is: ' || emp_sal);
 END; 
 
 -- 7. Create a new depts table as a copy of departments table.
@@ -46,38 +46,38 @@ create table depts as (select * from hr.departments)
 
 -- 8. Create a PL/SQL block that selects the maximum department ID in the depts table and stores it in the v_max_deptno variable. Display the maximum department ID.
 DECLARE
-  v_max_deptno depts.department_id%TYPE;
+    v_max_deptno depts.department_id%TYPE;
 BEGIN
-  select max(department_id) into v_max_deptno from depts;
-  dbms_output.put_line('Maximum department ID: ' || v_max_deptno);
+    select max(department_id) into v_max_deptno from depts;
+    dbms_output.put_line('Maximum department ID: ' || v_max_deptno);
 END; 
 
 -- 9. Create a PL/SQL block that inserts a new department into the depts table. Assign 'Education' to department_name. Add 10 to the maximum existing department ID to get a new department_id. The location can be NULL.
 DECLARE
-  v_max_deptno depts.department_id%TYPE;
+    v_max_deptno depts.department_id%TYPE;
 BEGIN
-  select max(department_id) into v_max_deptno from depts;
-  insert into depts
-  values (v_max_deptno + 10, 'Education', null, null);
+    select max(department_id) into v_max_deptno from depts;
+    insert into depts
+    values (v_max_deptno + 10, 'Education', null, null);
 END; 
 
 -- 10. Create a PL/SQL block that updates the location_id to 3000 for the new department. Use the local variable v_dept_id to update the row.
 DECLARE
-  v_max_deptno depts.department_id%TYPE;
-  v_dept_id depts.department_id%TYPE;
+    v_max_deptno depts.department_id%TYPE;
+    v_dept_id depts.department_id%TYPE;
 BEGIN
-  select max(department_id) into v_max_deptno from depts;
-  update depts set location_id=3000 
-  where department_id=v_max_deptno;
+    select max(department_id) into v_max_deptno from depts;
+    update depts set location_id=3000 
+    where department_id=v_max_deptno;
 END; 
 
 -- 11. Write  a DELETE statement to delete the department that you added.
 DECLARE
-  v_max_deptno depts.department_id%TYPE;
+    v_max_deptno depts.department_id%TYPE;
 BEGIN
-  select max(department_id) into v_max_deptno from depts;
-  delete from depts 
-  where department_id=v_max_deptno;
+    select max(department_id) into v_max_deptno from depts;
+    delete from depts 
+    where department_id=v_max_deptno;
 END; 
 
 -- 12. Create an anonymous block with a variable and the most complex form of the conditional expression. Using basic  LOOP, FOR and WHILE loop, create an anonymous block to print in the loop:
@@ -86,44 +86,44 @@ END;
     -- variable equals  3
     -- variable equals  4
 DECLARE
-  var1 NUMBER;
+    var1 NUMBER;
 BEGIN
-  dbms_output.put_line('LOOP');
-  var1 := 1;
-  loop
-    dbms_output.put_line('variable equals ' || var1);
-    var1 := var1 + 1;
-    exit when var1 = 5;
-  end loop;
+    dbms_output.put_line('LOOP');
+    var1 := 1;
+    loop
+        dbms_output.put_line('variable equals ' || var1);
+        var1 := var1 + 1;
+        exit when var1 = 5;
+    end loop;
 
-  dbms_output.put_line('WHILE');
-  var1 := 1;
-  while var1 < 5 loop
-    dbms_output.put_line('variable equals ' || var1);
-    var1 := var1 + 1;
-  end loop;
+    dbms_output.put_line('WHILE');
+    var1 := 1;
+    while var1 < 5 loop
+        dbms_output.put_line('variable equals ' || var1);
+        var1 := var1 + 1;
+    end loop;
 
-  dbms_output.put_line('FOR');
-  var1 := 1;
-  for var1 in 1..4 loop
-    dbms_output.put_line('variable equals ' || var1);
-  end loop;
+    dbms_output.put_line('FOR');
+    var1 := 1;
+    for var1 in 1..4 loop
+        dbms_output.put_line('variable equals ' || var1);
+    end loop;
 END; 
 
 -- 13. Declare an anonymous block looping from the value 3 to 7 and printing the value of the counter with comments: "start" for 3, "middle" for 5 and "end" for 7.
 DECLARE
-  var1 NUMBER;
+    var1 NUMBER;
 BEGIN
-  var1 := 1;
-  for var1 in 3..7 loop
-    if var1 = 3 then
-      dbms_output.put_line('start');
-    elsif var1 = 5 then
-      dbms_output.put_line('middle');
-    elsif var1 = 7 then
-      dbms_output.put_line('end');
-    end if;
-  end loop;
+    var1 := 1;
+    for var1 in 3..7 loop
+        if var1 = 3 then
+            dbms_output.put_line('start');
+        elsif var1 = 5 then
+            dbms_output.put_line('middle');
+        elsif var1 = 7 then
+            dbms_output.put_line('end');
+        end if;
+    end loop;
 END; 
 
 -- 14. Create a new emps table as a copy of employees table. Add a new column, stars, of VARCHAR2 data type and size 50.
@@ -132,17 +132,17 @@ alter table emps add stars VARCHAR2(50);
 
 -- 15. Create a PL/SQL block that inserts an asterisk in the stars column for every $1000 of the employee’s salary.
 DECLARE
-  emp_sal emps.salary%TYPE;
-  v_stars VARCHAR2(50);
+    emp_sal emps.salary%TYPE;
+    v_stars VARCHAR2(50);
 BEGIN
   for records in (select * from emps) loop
     emp_sal := records.salary;
     v_stars := '';
     
     loop 
-      exit when emp_sal < 1000;
-      v_stars := v_stars || '*';
-      emp_sal := emp_sal - 1000;
+        exit when emp_sal < 1000;
+        v_stars := v_stars || '*';
+        emp_sal := emp_sal - 1000;
     end loop;
 
     update emps 
--- database-programming/task-lab-05-pl-sql-cursors-and-handling-exceptions.sql
+++ database-programming/task-lab-05-pl-sql-cursors-and-handling-exceptions.sql
@@ -3,10 +3,29 @@
     -- Declare a variable v_countryid. Assign CA to v_countryid.
     -- In the declarative section, use the %ROWTYPE attribute and declare the v_country_record variable of type countries.
     -- In the executable section, get all the information from the countries table by using v_countryid. Display selected information about the country.
+DECLARE
+    TYPE r_countries IS RECORD (
+        country_id      hr.countries.country_id%TYPE,
+        country_name    hr.countries.country_name%TYPE,
+        region_id       hr.countries.region_id%TYPE );
+    v_countryid hr.countries.country_id%TYPE := 'CA';
+    v_country_record r_countries;
+  
+BEGIN
+  select * into v_country_record from hr.countries
+  where country_id = v_countryid;
+  
+  dbms_output.put_line( v_country_record.country_id || ' ' ||
+                        v_country_record.country_name || ' ' ||
+                        v_country_record.region_id);
+END; 
+
 -- 2. Create a PL/SQL block that does the following:
     -- In the declarative section, declare a variable v_deptno of type NUMBER and assign a value that holds the department ID.
     -- Declare a cursor, c_emp_cursor, that retrieves the last_name, salary, and manager_id of the employees working in the department specified in v_deptno.
     -- In the executable section, use the cursor FOR loop to operate on the data retrieved. If the salary of the employee is less than 5,000 and if the manager ID is either 101 or 124, display the message “<<last_name>> Due for a raise.” Otherwise, display the message “<<last_name>> Not due for a raise.”
+
+    
 -- 3. Create a PL/SQL block that determines the top n salaries of the employees. n should be passed as a variable in the block.
 -- 4. Write a PL/SQL block, which declares and uses cursors with parameters. In a loop, use a cursor to retrieve the department number and the department name from the departments table for a department whose department_id is less than 100. Pass the department number to another cursor as a parameter to retrieve from the employees table the details of employee last name, job, hire date, and salary of those employees whose employee_id is less than 120 and who work in that department.
 -- 5. Write a PL/SQL block to select the name of the employee with a given salary value. If the salary entered returns only one row, display employee’s name and the salary amount. If the salary entered does not return any rows or returns more than one row, handle the exception with appropriate exception handlers and messages.
