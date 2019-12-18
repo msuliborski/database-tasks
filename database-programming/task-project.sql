99929f2d308195090dbfd73bf2274276a093060d
Michal Suliborski <msuliborski@gmail.com>
2019-12-18 10:11:51 +0100

    prep to project

---
+++ database-programming/task-project.sql
@@ -0,0 +1,26 @@
+-- Design and implement a database system. Topic: Hotel database
+
+-- Note the following guidelines:
+-- Grade 3
+    -- - at least 5 tables should be defined.
+    -- - the database schema reflects all the attributes and dependencies of the “real” objects within the given topic.
+    -- - there are relationships between tables based on primary and foreign keys.
+    -- - sample data are provided for all the tables,
+    -- - at least 10 SQL queries for data preview should be defined; aggregate functions and SQL clauses should be considered,
+    -- - at least 3 procedures performing different operations should be defined (e.g. for adding, modification, and retrieving data from the database)
+    -- - functions that enable calculations should be defined (at least 2).
+
+-- Grade 4
+    -- - all requirements for the assessment of 3, plus:
+    -- - definitions of procedures and functions should prove your high database skills (complexity, "if" statements, loops, cursors, handling exceptions ...) and should differ in data they work on, operations they perform, and attributes they accept.
+
+-- Grade 5
+    -- - all requirements for the assessment of 4, plus:
+    -- - at least 3 tables should have triggers defined,
+    -- - additional database structures should be defined, e.g. packages, database scheduler, ...
+
+-- Note: No interface is required (except for standard database output).
+
+
+
+ 
