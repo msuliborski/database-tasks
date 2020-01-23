6745fb41698527698f7b3d3801a2d44abd733240
Michal Suliborski <msuliborski@gmail.com>
2020-01-23 14:39:48 +0100

    test set a done

--- database-programming/test-lab-set-a.sql
+++ database-programming/test-lab-set-a.sql
@@ -2,9 +2,18 @@
 -- SET A
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
+
+
 -- 1
 -- Stworzyć blok anonimowy, który przy użyciu kursora dla każdego pracownika z tabeli pracownicy wypisuje na ekranie tekst:
 -- 'Pracownik NAZWISKO pracuje w dziale NAZWA_DZIALU', gdzie NAZWISKO pochodzi z tabeli pracownicy, natomiast NAZWA_DZIALU pochodzi z tabeli działy
+declare
+  cursor c_emp is select last_name, department_name from employees e join departments d on e.department_id = d.department_id;
+begin
+  FOR v_emp IN c_emp LOOP
+    dbms_output.put_line('Employee ' || v_emp.last_name || ' works in department ' || v_emp.department_name);
+  END LOOP;
+end;
 
 
 
@@ -12,12 +21,51 @@
 -- Stworzyć procedurę, która pobiera nazwisko pracownika jako parametr, zwiększa mu pensję o 10% i wypisuje komunikat informujący o tym, ze operacja się powiodła
 -- Jeżeli nie znajdzie takiego pracownika, to wypisuje odpowiedni komunikat poprzez obsługę błędów.
 -- W sekcji deklaratywnej procedury musi zostać zdefiniowany wyjątek, który musi zostać wygenerowany w ciele procedury i obsłużony w sekcji EXCEPTION procedury.
+create or replace procedure salary_raise(p_last_name employees.last_name%type) is
+  e_no_emp_found exception;
+  emp_count number(4);
+begin
+  select count(*) into emp_count from employees where last_name = p_last_name;
+  if emp_count = 0 then
+    raise e_no_emp_found;
+  end if;
+  update employees 
+  set salary = salary * 1.1;
+exception
+  when e_no_emp_found then
+    dbms_output.put_line('No employees found'); 
+end;
+
+exec salary_raise('King');
+exec salary_raise('Wrong last name');
 
 
 
 -- 3
 -- Stworzyć funkcję, która pobiera nazwę działu jako parametr, oblicza sumę zarobków dla podanego działu, wypisuje oraz zwraca obliczoną wartość.
 -- Jeżeli nie znajdzie takiego działu, to wypisuje odpowiedni komunikat poprzez obsługę błędów.
+create or replace function dep_sum(p_dep_name departments.department_name%type) 
+return employees.salary%type is
+  e_no_dep_found exception;
+  dep_count number(4);
+  dep_sum employees.salary%type;
+begin
+  select count(*) into dep_count from departments where department_name = p_dep_name;
+  if dep_count = 0 then
+    raise e_no_dep_found;
+  end if;
+
+  select sum(salary) into dep_sum from employees e join departments d on e.department_id = d.department_id where department_name = p_dep_name;
+  return dep_sum;
+exception
+    when e_no_dep_found then
+    dbms_output.put_line('No departments found'); 
+    return 0;
+end;
+
+select dep_sum('executive') from dual; 
+select dep_sum('wrong department name') from dual; 
+
 
 
 -- 4
@@ -25,199 +73,20 @@
 -- Tabelę danych historycznych można wygenerować za pomocą polecenia:
 -- CREATE TABLE PRACOWNICY_ARCHIWUM AS SELECT * FROM HR.employees;
 -- DELETE FROM PRACOWNICY_ARCHIWUM;
-
-
-
-
-
-
-
-
-
-
-
-
-
--- ANSWERS
--- by Michal
--- 1
-DECLARE
-CURSOR c_emp IS SELECT last_name, department_name FROM employees e JOIN departments d ON e.department_id = d.department_id;
-v_last_name VARCHAR2(20);
-v_dep_name VARCHAR2(20);
+create or replace trigger emp_change
+before update or delete on employees
+for each row
 begin
-  OPEN c_emp;
-  loop
-    FETCH c_emp INTO v_last_name, v_dep_name;
-    EXIT WHEN c_emp%NOTFOUND;
-    dbms_output.put_line(v_last_name || ' ' || v_dep_name);
-  end loop;
-  close c_emp;
-
+  insert into employees_archive (employee_id, first_name, last_name, email, phone_number, hire_date, job_id, salary, commission_pct, manager_id, department_id)
+  values (:old.employee_id, :old.first_name, :old.last_name, :old.email, :old.phone_number, :old.hire_date, :old.job_id, :old.salary, :old.commission_pct, :old.manager_id, :old.department_id);
 end;
 
--- 2
-CREATE OR REPLACE PROCEDURE EX2(p_last_name employees.last_name%type)
-IS
-ex exception;
-c NUMBER(10);
-BEGIN
-    SELECT count(*) INTO c from employees WHERE last_name = p_last_name;
-    IF c = 0 then
-     raise ex;
-    END IF;
-    update employees
-       set salary = 1.1 * salary
-     where last_name = p_last_name;
-    exception
-      when ex then
-        dbms_output.put_line('SHIEEET!');
-END;
+select * from employees where employee_id = 100;
+update employees set first_name = 'changed_name' where employee_id = 100;
+select * from employees where employee_id = 100;
+select * from employees_archive;
 
--- 3
-CREATE OR REPLACE FUNCTION t(p_dep_name departments.department_name%type)
-RETURN employees.salary%type
-IS
-sum_ employees.salary%type;
-dep_id departments.department_id%type;
-begin
-  SELECT department_id INTO dep_id FROM departments WHERE department_name = p_dep_name;
-  SELECT sum(salary) INTO sum_ FROM employees WHERE department_id = dep_id;
-  dbms_output.put_line(sum_);
-  return sum_;
-  exception
-    when no_data_found then
-      dbms_output.put_line('SHIEEET!');
-        return 0;
-end;
-
-
--- 4
-CREATE TABLE PRACOWNICY_ARCHIWUM AS SELECT * FROM HR.employees;
-
-CREATE OR REPLACE TRIGGER h
-BEFORE UPDATE OR DELETE ON employees
-FOR EACH ROW
-begin
-  insert into PRACOWNICY_ARCHIWUM 
-  values (:old.employee_id, :old.first_name, :old.last_name, :old.email,
-  :old.phone_number, :old.hire_date, :old.job_id, :old.salary, :old.commission_pct, :old.manager_id, :old.department_id);
-end;
-
-SELECT employee_id, first_name FROM HR.employees WHERE employee_id=197;
-UPDATE HR.employees SET first_name = 'Imie' WHERE employee_id=197;
-SELECT employee_id, first_name FROM HR.employees WHERE employee_id=197;
-SELECT employee_id, first_name FROM pracownicy_archiwum WHERE employee_id=197;
-
-
-
-
-
--- theirs
-
--- 1
-DECLARE
-  CURSOR kursor IS
-  SELECT last_name, department_name FROM HR.employees E, HR.departments D WHERE E.department_id = D.department_id;
-  nazwisko HR.employees.last_name%TYPE;
-  nazwa_dzialu HR.departments.department_name%TYPE;
-BEGIN
-  OPEN kursor;
-  LOOP
-  FETCH kursor INTO nazwisko, nazwa_dzialu;
-  IF kursor%NOTFOUND
-  THEN
-    EXIT;
-  END IF;	
-    DBMS_OUTPUT.put_line('Pracownik '|| nazwisko || ' pracuje w dziale ' || nazwa_dzialu );
-  END LOOP;
-  CLOSE kursor;
-END;
-
---2
-
--- wlasny wyjatek
-CREATE OR REPLACE PROCEDURE procedura   
-(nazwisko HR.employees.last_name%TYPE) IS
-  wyjatek EXCEPTION;
-  licznik NUMBER;
-BEGIN
-  SELECT COUNT(*) INTO licznik FROM HR.employees WHERE last_name = nazwisko;
-  IF licznik = 0
-  THEN
-	RAISE wyjatek;
-  ELSE
-    UPDATE HR.employees SET salary = 1.1 * salary WHERE last_name = nazwisko;
-    DBMS_OUTPUT.put_line('Pracownik '|| nazwisko || ' mial zwiekszona pensje o 10%.' );
-  END IF;
-EXCEPTION
-  WHEN wyjatek
-  THEN
-	DBMS_OUTPUT.put_line('Pracownik '|| nazwisko || ' nie zostal odnaleziony.' );
-END;
-
--- wyjątek NO_DATA_FOUND
--- tego nie używać chyba, ale to jest logczine :c
-CREATE OR REPLACE PROCEDURE procedura   
-(nazwisko HR.employees.last_name%TYPE) IS
-BEGIN
-  UPDATE HR.employees SET salary = 1.1 * salary WHERE last_name = nazwisko;
-  DBMS_OUTPUT.put_line('Pracownik '|| nazwisko || ' mial zwiekszona pensje o 10%.' )
-EXCEPTION
-  WHEN NO_DATA_FOUND
-  THEN
-	DBMS_OUTPUT.put_line('Pracownik '|| nazwisko || ' nie zostal odnaleziony.' );
-END;
-
-
-EXECUTE procedura('Leo');
-
-SELECT salary FROM HR.employees WHERE last_name='Bull';
-EXECUTE procedura('Bull');
-SELECT salary FROM HR.employees WHERE last_name='Bull';
-
-ROLLBACK;
-
-
--- 3
-CREATE OR REPLACE FUNCTION funkcja   
-(nazwa_dzialu HR.departments.department_name%TYPE)
-RETURN NUMBER IS
-  nr_dzialu HR.employees.department_id%TYPE;
-  suma HR.employees.salary%TYPE := 0;
-BEGIN
-  SELECT department_id INTO nr_dzialu FROM HR.departments WHERE department_name = nazwa_dzialu;
-  SELECT SUM(salary) INTO suma FROM HR.employees WHERE department_id = nr_dzialu;
-  DBMS_OUTPUT.put_line('Suma zarobkow oddzialu ' || nazwa_dzialu || ' wynosi ' || suma);
-  RETURN suma;
-EXCEPTION
-	WHEN NO_DATA_FOUND
-	THEN
-		DBMS_OUTPUT.put_line('Dzial '|| nazwa_dzialu || ' nie zostal odnaleziony.' );
-    RETURN 0;
-END;
-
-EXECUTE DBMS_OUTPUT.put_line(funkcja('Nieznany oddzial'));
-EXECUTE DBMS_OUTPUT.put_line(funkcja('IT'));
-
-ROLLBACK;
-
-
---4
-CREATE TABLE pracownicy_archiwum AS SELECT * FROM HR.employees;
-DELETE FROM pracownicy_archiwum;
-
-CREATE OR REPLACE TRIGGER wyzwalacz
-BEFORE update OR delete ON HR.employees FOR EACH ROW
-BEGIN
-  INSERT INTO pracownicy_archiwum VALUES (:old.employee_id, :old.first_name, :old.last_name, :old.email,
-  :old.phone_number, :old.hire_date, :old.job_id, :old.salary, :old.commission_pct, :old.manager_id, :old.department_id);
-END;
-
-
-SELECT employee_id, first_name FROM HR.employees WHERE employee_id=197;
-UPDATE HR.employees SET first_name = 'Imie' WHERE employee_id=197;
-SELECT employee_id, first_name FROM HR.employees WHERE employee_id=197;
-SELECT employee_id, first_name FROM pracownicy_archiwum WHERE employee_id=197;
-
-ROLLBACK;
+select * from employees where employee_id = 197;
+delete from employees where employee_id = 197;
+select * from employees where employee_id = 197;
+select * from employees_archive;
